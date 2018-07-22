d139340166e075560eb15019ddcfe5429e6e68d0
KendallU <kendallunion@gmail.com>
2018-07-22 13:42:54 -0500

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name, age, and location?");
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type name here:");
+            string myName;
+            myName = Console.ReadLine();
+
+            Console.WriteLine("What is your Age");
+            Console.Write("Type age here:");
+            string myAge;
+            myAge = Console.ReadLine();
+
+            Console.WriteLine("What is your Location?");
+            Console.Write("Type location here:");
+            string myLocation;
+            myLocation = Console.ReadLine();
+
+            Console.WriteLine("Hello, "  + myName  +  " I am "  +  myAge  +  " years old, and I live in "  + myLocation);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
